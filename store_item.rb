5df68f4e49da7ae63108bfeb5f662bdbe10703b7
Stacie Taitague <stacie.taitague@gmail.com>
2021-02-17 14:36:18 -0500

    adds methods, attr, and values

--- store_item.rb
+++ store_item.rb
@@ -2,12 +2,12 @@ itemA = {:item => "photograph", :price => 50, :inventory => 12}
 itemB = {item: "postcard", price: 10, inventory: 40}
 itemC = {:item => "gift bag", :price => 25, :inventory => 35}
 
-puts itemA[:item]
+# puts itemA[:item]
 
 # Using Class for Information
 
 class Items
-  attr_reader :photograph, :price
+  attr_reader :item, :price
   attr_writer :inventory
 
   def initialize (input_item, input_price, input_inventory)
@@ -16,4 +16,12 @@ class Items
     @inventory = input_inventory
   end
 
+  def print_info
+    p @item, @price, @inventory
+  end
+
 end
+
+itemA = Items.new("photograph", 50, 12)
+
+itemA.print_info
