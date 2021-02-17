9ee1024dae773e315178d687598e42b1dff67ae7
Stacie Taitague <stacie.taitague@gmail.com>
2021-02-17 14:58:37 -0500

    adds input options hash

--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,6 @@
-itemA = {:item => "photograph", :price => 50, :inventory => 12}
-itemB = {item: "postcard", price: 10, inventory: 40}
-itemC = {:item => "gift bag", :price => 25, :inventory => 35}
+# itemA = {:item => "photograph", :price => 50, :inventory => 12}
+# itemB = {item: "postcard", price: 10, inventory: 40}
+# itemC = {:item => "gift bag", :price => 25, :inventory => 35}
 
 # puts itemA[:item]
 
@@ -10,10 +10,10 @@ class Items
   attr_reader :item, :price
   attr_writer :inventory
 
-  def initialize (input_item, input_price, input_inventory)
-    @item = input_item
-    @price = input_price
-    @inventory = input_inventory
+  def initialize (input_options)
+    @item = input_options[:input_item]
+    @price = input_options[:input_price]
+    @inventory = input_options[:input_inventory]
   end
 
   def print_info
@@ -22,6 +22,7 @@ class Items
 
 end
 
-itemA = Items.new("photograph", 50, 12)
+itemA = Items.new({input_item: "photograph", input_price: 50, input_inventory: 12})
 
 itemA.print_info
+
