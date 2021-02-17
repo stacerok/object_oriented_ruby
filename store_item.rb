a2cfdaec90949f8a69a831b6c6a14b4a5a3a394e
Stacie Taitague <stacie.taitague@gmail.com>
2021-02-17 13:29:58 -0500

    added puts to test code

--- store_item.rb
+++ store_item.rb
@@ -2,3 +2,4 @@ itemA = {:item => "photograph", :price => 50, :inventory => 12}
 itemB = {item: "postcard", price: 10, inventory: 40}
 itemC = {:item => "gift bag", :price => 25, :inventory => 35}
 
+puts itemA[:item]
