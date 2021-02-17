734b6652197a6c3ff71ada088c60b106d1e74866
Stacie Taitague <stacie.taitague@gmail.com>
2021-02-17 13:25:52 -0500

    adds hashes of items

---
+++ store_item.rb
@@ -0,0 +1,4 @@
+itemA = {:item => "photograph", :price => 50, :inventory => 12}
+itemB = {item: "postcard", price: 10, inventory: 40}
+itemC = {:item => "gift bag", :price => 25, :inventory => 35}
+
